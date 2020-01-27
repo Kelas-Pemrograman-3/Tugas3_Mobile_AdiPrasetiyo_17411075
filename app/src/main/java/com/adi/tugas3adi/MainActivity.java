58d66183041ac63674354f41e90804b409491764
Adiprasetiyo1 <adi.17411075@student.ubl.ac.id>
2020-01-27 13:54:36 +0700

    first commit

---
+++ app/src/main/java/com/adi/tugas3adi/MainActivity.java
@@ -0,0 +1,97 @@
+package com.adi.tugas3adi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiZHdpa3lqdWxsaWFuIiwiYSI6ImNrMzlpeDA3NDAzbDAzbHAxaHZ2ZmJkdjkifQ.YpzAFSc9Qhpumta5uVxurQ");
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+
+                    }
+                });
+                marker(-5.397140, 105.266792, mapboxMap);
+                marker(-5.274454, 105.187679, mapboxMap);
+                marker(-5.385560, 105.299350, mapboxMap);
+                marker(-5.373360, 105.225950, mapboxMap);
+                marker(-5.384630, 105.260437, mapboxMap);
+            }
+        });
+
+    }
+
+    public void marker(double lat, double lon, MapboxMap mapboxMap){
+        MarkerOptions options = new MarkerOptions();
+        options.setTitle("Posisi Sekarang");
+        options.position(new LatLng(lat, lon));
+        mapboxMap.addMarker(options);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+}
